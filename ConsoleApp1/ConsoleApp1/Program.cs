4d75657d8c127bb6c3b125d0e27ce7ce112c93f8
liana mazayan <mazayanlian@gmail.com>
2019-07-25 22:47:52 +0400

    newOne

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,33 @@ namespace ConsoleApp1
 {
     class Program
     {
+       static void sum(ref int x, ref int y)
+        {
+            if (x > 0)
+            {
+                x--;
+                y++;
+                sum(ref x, ref y);
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+
+
+        }
         static void Main(string[] args)
         {
-        }
+
+        int a = 5;
+        int b = 20;
+        sum(ref a, ref b);
+
+
+
+   
+    }
+
+
     }
 }
